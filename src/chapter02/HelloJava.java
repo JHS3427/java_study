44bf935069c6f908f56dc45382fe74b37c0d9c73
JHS3427 <jhs34276225@naver.com>
2025-06-24 13:53:08 +0900

    Initial commit

---
+++ src/chapter02/HelloJava.java
@@ -0,0 +1,12 @@
+package chapter02;
+
+public class HelloJava { //-퍼블릭은 변경이나 생략 가능
+	
+	//메인 메소드(메인 함수) - 실행을 위한 필수 메소드
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome to JAVAWorld!!!!");
+
+	}
+
+}
